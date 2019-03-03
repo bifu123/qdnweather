0d6e5b6cf6aedb1428d48c9e124eb5ad758536be
bifu123 <415135222@qq.com>
2019-03-04 05:46:03 +0800

    Update mainclass.py

--- class/mainclass.py
+++ class/mainclass.py
@@ -2,16 +2,18 @@
 # -*- coding: UTF-8 -*-
 class allMethod(object):
     """docstring for ClassName"""
-    def __init__(self, message):
-        self.message=message
+    def __init__(self, msg):
+        self.msg=msg
         
     def WriteTo(self):
         import os
         fi=open('log.txt',mode='a')
-        fi.write('%s'%(self.message))
+        fi.write('%s'%(self.msg))
         fi.write('\n')
         fi.close()
         #os.system('sudo chmod 777 log.txt')
+
+    #x=WriteToLog().WriteTo('dog')
 
     def mailto(self):
         import smtplib
@@ -19,14 +21,21 @@ class allMethod(object):
         from email.mime.text import MIMEText
         from email.mime.image import MIMEImage
         from email.mime.application import MIMEApplication
+        #163邮箱服务器地址
+        mail_host = 'smtp.126.com'  
+        #163用户名
+        mail_user = 'cbfddt0000'  
+        #密码(部分邮箱为授权码、网易就是授权码，不是登陆密码，一定要注意) 
+        mail_pass = '******'   
+        #邮件发送方邮箱地址
+        sender = 'cbfddt0000@126.com'  
+        #邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
+        receivers = ['415135222@qq.com','515826@qq.com']
 
+        #设置email信息
+        #邮件内容设置
+        message = MIMEMultipart('related')
 
-        HOST = "smtp.126.com"
-        SUBJECT = u"官网业务服务质量周报"
-        TO = "415135222@qq.com"
-        FROM = "cbfddt0000@126.com"
-
-        msg = MIMEMultipart('related')
         def addimg(src,imgid):
             fp = open(src, 'rb')
             msgImage = MIMEImage(fp.read())
@@ -34,28 +43,33 @@ class allMethod(object):
             msgImage.add_header('Content-ID', imgid)
             return msgImage
 
-        msg.attach(addimg("/home/test/python/test/weekly.png","weekly"))
-
-        
-        msgtext = MIMEText(self.message,"html","utf-8")
-        msg.attach(msgtext)
-
+        message.attach(addimg("weekly.png","weekly"))
+        mialBody = MIMEText(self.msg,'html','utf-8')
+        message.attach(mialBody)
 
         attach = MIMEApplication(open('曾柏超的简历.doc','rb').read())
         attach.add_header('Content-Disposition', 'attachment', filename="曾柏超的简历.doc")
-        msg.attach(attach)
+        message.attach(attach)
 
 
-        msg['Subject'] = SUBJECT
-        msg['From']=FROM
-        msg['To']=TO
+        #邮件主题       
+        message['Subject'] = '爬网结果通知' 
+        #发送方信息
+        message['From'] = sender 
+        #接受方信息     
+        message['To'] = ",".join(receivers) #网易邮箱的收件人是字符类型，用,分隔，所以必须把list转为字符，要不就只能在上面定义为字符串，再发送方法那里再搞成LIST
+
+        #登录并发送邮件
         try:
-            server = smtplib.SMTP()
-            server.connect(HOST,"25")
-            #server.starttls()
-            server.login("cbfddt0000@126.com","Shift962512")
-            server.sendmail(FROM, TO, msg.as_string())
-            server.quit()
-            print("邮件发送成功！")
-        except Exception as e:
-            print("失败："+str(e))
+            smtpObj = smtplib.SMTP() 
+            #连接到服务器
+            smtpObj.connect(mail_host,25)
+            #登录到服务器
+            smtpObj.login(mail_user,mail_pass) 
+            #发送
+            smtpObj.sendmail(sender,receivers,message.as_string()) #发送方法中的收件人是list
+            #退出
+            smtpObj.quit() 
+            print('success')
+        except smtplib.SMTPException as e:
+            print('error',e) #打印错误
